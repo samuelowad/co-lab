0c5cbea72fa9631064424047a80fb08c8210fa00
samuelowad <samuelowolabi330@gmail.com>
2020-04-28 21:05:42 +0100

    ok

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 
 module.exports = {
     mailer: {},
-    dbConnection: `mongodb+srv://Samuelowad:igrrq6pFC3fugflk@cluster0-c8daj.mongodb.net/collab?retryWrites=true&w=majority`
+    dbConnection: `mongodb+srv://Samuelowad:igrrq6pFC3fugflk@cluster0-c8daj.mongodb.net/collab?retryWrites=true&w=majority`,
     sessionKey: 'NodeCollab'
 }
